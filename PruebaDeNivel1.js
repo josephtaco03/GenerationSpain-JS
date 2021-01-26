a5d7a8bf8a1f614b42857400987b870cfb35f2a8
josephtaco03 <76428003+josephtaco03@users.noreply.github.com>
2021-01-26 12:57:08 +0100

    Updated

---
+++ PruebaDeNivel1.js
@@ -0,0 +1,9 @@
+
+function esPalindromo(cadena) {
+    let array = cadena.split("");
+    let reverse = array.reverse();
+
+    return cadena == reverse.join("") ? "Si es" : "No es"
+}
+
+console.log(esPalindromo("geeksforgeeks"))
